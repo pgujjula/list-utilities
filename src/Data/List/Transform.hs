50e815e96711b290f8abf8e59d458527c7a9e7c4
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2020-04-21 23:50:20 +0000

    takeEvery

--- src/Data/List/Transform.hs
+++ src/Data/List/Transform.hs
@@ -1,1 +1,28 @@
-module Data.List.Transform () where
+{-|
+Module      : Data.List.Digit
+Description : Transform lists to other lists.
+Copyright   : (c) Preetham Gujjula, 2020
+License     : GPL-3
+Maintainer  : preetham.gujjula@gmail.com
+Stability   : experimental
+
+Transform lists to other lists.
+-}
+module Data.List.Transform
+  ( takeEvery
+  ) where
+
+{-| @takeEvery n xs@ is a list of every nth element of xs
+
+    __Precondition:__ @n@ must be positive.
+
+    >>> takeEvery 3 [1..10]
+    [3, 6, 9]
+    >>> (takeEvery 1 [1..10]) == [1..10]
+    True
+-}
+takeEvery :: Int -> [a] -> [a]
+takeEvery n xs =
+  case drop (n - 1) xs of
+    []     -> []
+    (y:ys) -> y : takeEvery n ys
--- test/Data/List/TransformSpec.hs
+++ test/Data/List/TransformSpec.hs
@@ -1,6 +1,32 @@
 module Data.List.TransformSpec (spec) where
 
-import Test.Hspec (Spec, shouldBe, it)
+import Test.Hspec            (Spec, describe, it, shouldBe)
+import Test.Hspec.QuickCheck (modifyMaxSuccess)
+
+import Data.List.Transform   (takeEvery)
+
+numTests :: Int
+numTests = 1000
 
 spec :: Spec
-spec = it "test" $ (1 + 1) `shouldBe` (2 :: Int)
+spec = modifyMaxSuccess (const numTests) $ do
+       describe "takeEvery" takeEverySpec
+
+takeEverySpec :: Spec
+takeEverySpec = do
+  it "n = 1, empty list" $
+    takeEvery 1 ([] :: [Int]) `shouldBe` []
+  it "n = 1, finite list" $ do
+    let xs = [1..10] :: [Int]
+    takeEvery 1 xs `shouldBe` xs
+  it "n = 1, prefix of infinite list" $ do
+    let xs = [1..] :: [Int]
+    take 10 (takeEvery 1 xs) `shouldBe` take 10 xs
+
+  it "n = 3, empty list" $
+    takeEvery 3 ([] :: [Int]) `shouldBe` []
+  it "n = 3, finite list" $
+    takeEvery 3 [1..10] `shouldBe` ([3, 6, 9] :: [Int])
+  it "n = 3, prefix of infinite list" $ do
+    let xs = [1..] :: [Int]
+    take 10 (takeEvery 3 xs) `shouldBe` take 10 (map (*3) xs)

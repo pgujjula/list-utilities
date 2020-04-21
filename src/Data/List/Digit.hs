bd84d1c1a932f9636a8fd8579bae0c4ab753e182
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2020-04-21 20:05:10 +0000

    temp

--- src/Data/List/Digit.hs
+++ src/Data/List/Digit.hs
@@ -1,4 +1,14 @@
-module Data.List.Digit (numDigits) where
+module Data.List.Digit
+  ( numDigits
+  , sumDigits
+  , fromDigits
+  , toDigits
+  ) where
+
+import Data.Char (chr, ord)
+
+ordZero :: Int
+ordZero = ord '0'
 
 {-|
     @numDigits n@ is the number of digits in @n@.
@@ -11,7 +21,48 @@ module Data.List.Digit (numDigits) where
     5
 -}
 numDigits :: (Integral a) => a -> Int
-numDigits = numDigitsAbs . abs
-  where numDigitsAbs n
-          | n < 10    = 1
-          | otherwise = 1 + (numDigitsAbs $ n `div` 10)
+numDigits = length . show . toInteger . abs
+
+{-|
+    @sumDigits n@ is the sum of the digits in @n@.
+
+    >>> sumDigits 2938475
+    38
+    >>> sumDigits 0
+    0
+    >>> sumDigits (-12)
+    3
+-}
+sumDigits :: (Integral a) => a -> Int
+sumDigits = sum . toDigits . abs
+
+{-|
+    @toDigits n@ is a list of the digits in @n@.
+
+    __Precondition:__ @n@ must be nonnegative, so that @toDigits@ is the
+    inverse of @fromDigits@.
+
+    >>> toDigits 2338475
+    [2, 3, 3, 8, 4, 7, 5]
+    >>> toDigits 0
+    [0]
+-}
+toDigits :: (Integral a) => a -> [Int]
+toDigits = map (\x -> ord x - ordZero) . show . toInteger
+
+{-|
+    @fromDigits xs@ is converts a list of digits @xs@ to an integer.
+
+    __Precondition:__ All elements of @xs@ are in @[0..9]@.
+
+    >>> fromDigits [1, 7, 2, 9]
+    1729
+    >>> fromDigits []
+    0
+    >>> fromDigits [0, 0, 0]
+    0
+    >>> fromDigits [0, 4, 2]
+    42
+-}
+fromDigits :: (Integral a) => [Int] -> a
+fromDigits = fromInteger . read . map (\x -> chr (x + ordZero))

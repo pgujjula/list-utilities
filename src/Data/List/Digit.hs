9588889c459ae707a7eeabe8073e99bc381587d1
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2020-04-21 07:01:06 +0000

    numDigits test

--- src/Data/List/Digit.hs
+++ src/Data/List/Digit.hs
@@ -1,1 +1,17 @@
-module Data.List.Digit () where
+module Data.List.Digit (numDigits) where
+
+{-|
+    @numDigits n@ is the number of digits in @n@.
+
+    >>> numDigits 2938475
+    7
+    >>> numDigits 0
+    1
+    >>> numDigits (-38417)
+    5
+-}
+numDigits :: (Integral a) => a -> Int
+numDigits = numDigitsAbs . abs
+  where numDigitsAbs n
+          | n < 10    = 1
+          | otherwise = 1 + (numDigitsAbs $ n `div` 10)
--- test/Data/List/DigitSpec.hs
+++ test/Data/List/DigitSpec.hs
@@ -1,6 +1,56 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.List.DigitSpec (spec) where
 
-import Test.Hspec (Spec)
+import Control.Monad         (forM_)
+
+import System.Random         (Random)
+import Test.Hspec            (Spec, describe, it, shouldBe)
+import Test.Hspec.QuickCheck (modifyMaxSuccess)
+import Test.QuickCheck       (Gen, choose, forAll, oneof, resize,
+                              sized, (===))
+
+import Data.List.Digit       (numDigits)
+
+numTests :: Int
+numTests = 1000
+
+maxNumDigits :: Int
+maxNumDigits = 50
 
 spec :: Spec
-spec = undefined
+spec = modifyMaxSuccess (const numTests)
+     $ describe "numDigits" numDigitsSpec
+
+digits :: (Integral a) => [a]
+digits = [-9..9]
+
+-- Generate integers where the length in base 10 is uniformly distributed.
+-- Take care to prevent overflow.
+uniformLengthIntegralGen :: (Random a, Integral a) => Gen a
+uniformLengthIntegralGen = sized $ \n -> oneof $ map genLength [1..n]
+  where genLength i = oneof [positive, negative]
+          where lower = 10^(i - 1)
+                upper = 10^i - 1
+                positive = choose (lower, upper)
+                negative = choose (-upper, -lower)
+
+uniformLengthIntGen :: Gen Int
+uniformLengthIntGen = resize maxIntLength uniformLengthIntegralGen
+  where maxIntLength = (length $ show (maxBound :: Int)) - 1
+
+uniformLengthIntegerGen :: Gen Integer
+uniformLengthIntegerGen = resize maxNumDigits uniformLengthIntegralGen
+
+numDigitsSpec :: Spec
+numDigitsSpec = do
+  let testDigits ds = forM_ ds $ \x -> numDigits x `shouldBe` 1
+  it "on single-digit Ints equals 1"
+    $ testDigits (digits :: [Int])
+  it "on single-digit Integers equals 1"
+    $ testDigits (digits :: [Integer])
+
+  let numDigitsCorrect x = numDigits x === (length $ show $ abs x)
+  it "correct on Ints of random length"
+    $ forAll uniformLengthIntGen numDigitsCorrect
+  it "correct on Integers of random length"
+    $ forAll uniformLengthIntegerGen numDigitsCorrect

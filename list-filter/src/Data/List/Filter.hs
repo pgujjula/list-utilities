1e680a4accf31d4b40aa239b0619395a4840ade6
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2020-05-02 16:22:31 +0000

    add list-filter

---
+++ list-filter/src/Data/List/Filter.hs
@@ -0,0 +1,73 @@
+{-| Module      : Data.List.Filter
+    Description : Special takes and drops on lists
+    Copyright   : (c) Preetham Gujjula, 2020
+    License     : BSD3
+    Maintainer  : pgujjula+list-utilities@protonmail.com
+    Stability   : experimental
+
+Special takes and drops on lists.
+-}
+module Data.List.Filter (
+      takeEvery
+    , dropEvery
+    , takeUntil
+    , dropUntil
+    ) where
+
+{-| @takeEvery n xs@ is a list of every @n@th element of @xs@.
+
+    __Precondition:__ @n@ must be positive.
+
+    >>> takeEvery 3 [1..10]
+    [3, 6, 9]
+    >>> takeEvery 1 [1..10] == [1..10]
+    True
+-}
+takeEvery :: Int -> [a] -> [a]
+takeEvery step xs = compute validated
+  where compute ys = case drop (step - 1) ys of
+                         []    -> []
+                         y:ys' -> y : compute ys'
+        validated
+          | step > 0  = xs
+          | otherwise = error $ "Data.List.Transform.takeEvery: Step parameter "
+                             ++ "must be positive."
+
+dropEvery :: Int -> [a] -> [a]
+dropEvery = undefined
+
+{-| Take a list until a predicate is satisfied, and include the element
+    satisfying the predicate.
+
+    >>> takeUntil (== 5) [1..]
+    [1, 2, 3, 4, 5]
+    >>> takeUntil (== 7) [3, 2, 1]
+    [3, 2, 1]
+    >>> takeUntil undefined []
+    []
+
+    Note that @takeUntil@ on a nonempty list must always yield the first
+    element, and the implementation is lazy enough to take advantage of this
+    fact.
+
+    >>> head (takeUntil undefined [1..])
+    1
+-}
+takeUntil :: (a -> Bool) -> [a] -> [a]
+takeUntil _ []     = []
+takeUntil _ [x]    = [x]
+takeUntil f (x:xs) = x : (if f x then [] else takeUntil f xs)
+
+{-| Drop a list until a predicate is satisfied, and include the element
+    satisfying the predicate.
+
+    >>> dropUntil (== 5) [1..10]
+    [5, 6, 7, 8, 9, 10]
+    >>> dropUntil (< 0) [1, 2, 3]
+    []
+    >>> dropUntil undefined []
+    []
+-}
+dropUntil :: (a -> Bool) -> [a] -> [a]
+dropUntil _ []     = []
+dropUntil f (x:xs) = if f x then x:xs else dropUntil f xs
---
+++ list-filter/test/Data/List/FilterSpec.hs
@@ -0,0 +1,63 @@
+module Data.List.FilterSpec where
+
+import Data.List.Filter (takeEvery, takeUntil, dropUntil)
+
+import Test.Hspec (Spec, shouldBe, describe, it)
+
+empty :: [Integer]
+empty = []
+
+singletonUndef :: [Integer]
+singletonUndef = [undefined]
+
+spec :: Spec
+spec = do
+    describe "takeEvery" takeEverySpec
+    describe "takeUntil" takeUntilSpec
+    describe "dropUntil" dropUntilSpec
+
+takeEverySpec :: Spec
+takeEverySpec = do
+    it "n = 1, empty list" $
+        takeEvery 1 empty `shouldBe` empty
+    it "n = 1, finite list" $
+        takeEvery 1 [1..10] `shouldBe` [1..10]
+    it "n = 1, prefix of infinite list" $
+        take 10 (takeEvery 1 [1..]) `shouldBe` take 10 [1..10]
+
+    it "n = 3, empty list" $
+        takeEvery 3 empty `shouldBe` []
+    it "n = 3, finite list" $
+        takeEvery 3 [1..10] `shouldBe` [3, 6, 9]
+    it "n = 3, prefix of infinite list" $
+        take 10 (takeEvery 3 [1..]) `shouldBe` take 10 [3, 6..]
+
+takeUntilSpec :: Spec
+takeUntilSpec = do
+    it "empty list, undefined function" $
+        takeUntil undefined empty `shouldBe` empty
+    it "singleton list, undefined function" $
+        length (takeUntil undefined singletonUndef) `shouldBe` 1
+    it "finite list, undefined function" $
+        head (takeUntil undefined [1, 2, 3]) `shouldBe` 1
+    it "finite list" $
+        takeUntil (>= 5) [1..10] `shouldBe` [1..5]
+    it "finite list, always True" $
+        takeUntil (const True) [1..10] `shouldBe` [1]
+    it "finite list, always False" $
+        takeUntil (const False) [1..10] `shouldBe` [1..10]
+    it "infinite list" $
+        takeUntil (>= 5) [1..] `shouldBe` [1..5]
+
+dropUntilSpec :: Spec
+dropUntilSpec = do
+    it "empty list, undefined function" $
+        dropUntil undefined empty `shouldBe` empty
+    it "finite list" $
+        dropUntil (== 5) [1..10] `shouldBe` [5..10]
+    it "finite list, always True" $
+        dropUntil (const True) [1..10] `shouldBe` [1..10]
+    it "finite list, always False" $
+        dropUntil (const False) [1..10] `shouldBe` []
+    it "infinite list" $
+        take 6 (dropUntil (== 5) [1..]) `shouldBe` [5..10]

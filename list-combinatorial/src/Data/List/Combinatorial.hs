c1a8ffd392976e38ff00853d6d5abc42bce8c89b
Preetham Gujjula <pgujjula+gitcommit@protonmail.com>
2020-05-03 11:55:38 -0500

    flesh out

--- list-combinatorial/src/Data/List/Combinatorial.hs
+++ list-combinatorial/src/Data/List/Combinatorial.hs
@@ -11,21 +11,128 @@ module Data.List.Combinatorial (
       rotate
     , rotations
     , combinationsOfSize
-    , permutationsOfSize 
+    , permutationsOfSize
     , derangements
     ) where
 
+import Data.List (inits, tails)
+
+{-| Rotate a list by an offset. Positive offset is left rotation, negative is
+    right.
+
+    >>> rotate 2 [1..6]
+    [3, 4, 5, 6, 1, 2]
+    >>> rotate (-2) [1..6]
+    [5, 6, 1, 2, 3, 4]
+    >>> rotate 0 [1..6]
+    [1, 2, 3, 4, 5, 6]
+
+    @rotate@ also works efficiently even if the offset is very large, by
+    reducing it modulo the length of the list.
+
+    >>> rotate (60000000000000 + 2) [1..6]
+    [3, 4, 5, 6, 1, 2]
+
+    @rotate@ can do zero- and left- shifts on infinite lists, but right-shifts
+    will diverge.
+
+    >>> take 6 $ rotate 2 [1..]  -- This works
+    [3, 4, 5, 6, 7, 8]
+    >>> take 6 $ rotate (-2) [1..]  -- This diverges
+-}
 rotate :: Int -> [a] -> [a]
-rotate = undefined
+rotate _ [] = []
+rotate n xs = zs ++ ys
+  where
+    (ys, zs) = splitAt nModLength xs
+    nModLength | n < 0     = n `mod` length xs
+               | otherwise = n `rem` lengthTo n xs
 
+    -- The length of an list, up to a maximum mx. The length of any list
+    -- with length longer than m is reported as mx + 1.
+    lengthTo :: Int -> [a] -> Int
+    lengthTo mx = sum . take (mx + 1) . map (const 1)
+
+{-| All left-rotations of a list. Works with infinite lists.
+
+    >>> rotations [1, 2, 3, 4]
+    [[1, 2, 3, 4], [2, 3, 4, 1], [3, 4, 1, 2], [4, 1, 2, 3]]
+    >>> take 4 . map (take 4) $ rotations [1..]
+    [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]]
+    >>> rotations []
+    [[]]
+-}
 rotations :: [a] -> [[a]]
-rotations = undefined
+rotations [] = [[]]
+rotations xs = init $ zipWith (++) (tails xs) (inits xs)
 
+{-| All combinations of given size of the list. Elements will retain their
+    relative order, and if the original list is ordered, then the combinations
+    will also be ordered. Note that if the original list contains duplicates,
+    then the combinations will also contain duplicates--elements are not
+    evaluated, and are treated as if they are all distinct.
+
+    This function will not hang on infinite lists, but following the ordering
+    rules above means some combinations will never be produced, even though the
+    combinations are enumerable if we used a different ordering.
+
+    >>> combinationsOfSize 2 [1, 2, 3, 4]
+    [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+    >>> combinationsOfSize 0 [1, 2, 3, 4]
+    [[]]
+    >>> combinationsOfSize 4 [1, 2, 3, 4]
+    [[1, 2, 3, 4]]
+    >>> take 5 $ combinationsOfSize 3 [1..]
+    [[1, 2, 3], [1, 2, 4], [1, 2, 5], [1, 2, 6], [1, 2, 7]]
+-}
 combinationsOfSize :: Int -> [a] -> [[a]]
-combinationsOfSize = undefined
+combinationsOfSize n xs
+  | n < 0   = []
+  | n == 0  = [[]]
+  | otherwise = case xs of
+                  [] -> []
+                  (x:xs') -> map (x:) (combinationsOfSize (n - 1) xs')
+                          ++ combinationsOfSize n xs'
 
+{-| All permutations of given size of the list. If the original list is sorted,
+    permutations are returned in sorted order.
+
+    This function will not hang on infinite lists, but following the ordering
+    rules above means some permutations will never be produced, even though the
+    permutations are enumerable if we used a different ordering.
+
+    >>> permutationsOfSize 2 [1, 2, 3]
+    [[1, 2], [1, 3], [2, 1], [2, 3], [3, 1], [3, 2]]
+    >>> permutationsOfSize 0 [1, 2, 3]
+    [[]]
+-}
 permutationsOfSize :: Int -> [a] -> [[a]]
-permutationsOfSize = undefined
+permutationsOfSize n xs
+    | n < 0  = []
+    | n == 0 = [[]]
+    | otherwise = case xs of
+                  [] -> []
+                  _  -> concat $ zipWith (\x -> map (x:)) xs subPermutations
+  where
+    deleteEach ys = zipWith (++) (inits ys) (tail $ tails ys)
+    subPermutations = map (permutationsOfSize (n - 1)) (deleteEach xs)
 
+{-| Permutations where no element ends up in its original place. If the original
+    list is ordered, derangements are returned in sorted order.
+
+    This function will not hang on infinite lists, but following the ordering
+    rules above means some derangements will never be produced, even though the
+    derangements are enumerable if we used a different ordering.
+-}
 derangements :: [a] -> [[a]]
-derangements = undefined
+derangements xs = ds
+  where
+    index = zip [0..]
+    getIndices = map fst
+    stripIndices = map snd
+    derangedIndices = and . zipWith (/=) [(0 :: Int)..]
+
+    xs' = index xs
+    ps' = permutationsOfSize (length xs') xs'
+    ds' = filter (derangedIndices . getIndices) ps'
+    ds = map stripIndices ds'
--- list-combinatorial/test/Data/List/CombinatorialSpec.hs
+++ list-combinatorial/test/Data/List/CombinatorialSpec.hs
@@ -1,6 +1,11 @@
 module Data.List.CombinatorialSpec (spec) where
 
-import Test.Hspec (Spec, describe)
+import Control.Monad           (forM_, (>=>))
+import Data.List               (permutations, sort)
+import Test.Hspec              (Spec, describe, it, shouldBe)
+
+import Data.List.Combinatorial (combinationsOfSize, derangements,
+                                permutationsOfSize, rotate, rotations)
 
 spec :: Spec
 spec = do
@@ -11,16 +16,102 @@ spec = do
     describe "derangements" derangementsSpec
 
 rotateSpec :: Spec
-rotateSpec = undefined
+rotateSpec = do
+    it "empty list, 0 offset" $
+        rotate 0 [] `shouldBe` ([] :: [()])
+    it "empty list, positive offset" $
+        rotate 3 [] `shouldBe` ([] :: [()])
+    it "empty list, negative offset" $
+        rotate (-2) [] `shouldBe` ([] :: [()])
+
+    it "singleton list, 0 offset" $
+        rotate 0 [1] `shouldBe` [1]
+    it "singleton list, positive offset" $
+        rotate 2 [1] `shouldBe` [1]
+    it "singleton list, negative offset" $
+        rotate (-2) [1] `shouldBe` [1]
+
+    let xs = [1..6]
+    it "finite list, 0 offset" $
+        rotate 0 xs `shouldBe` xs
+    it "finite list, positive offset" $
+        rotate 2 xs `shouldBe` [3, 4, 5, 6, 1, 2]
+    it "finite list, negative offset" $
+        rotate (-2) xs `shouldBe` [5, 6, 1, 2, 3, 4]
+
+    it "finite list, positive offset equals length" $
+        rotate 6 xs `shouldBe` xs
+    it "finite list, negative offset equals negative length" $
+        rotate (-6) xs `shouldBe` xs
+
+    it "finite list, positive offset greater than length" $
+        rotate 10 xs `shouldBe` [5, 6, 1, 2, 3, 4]
+    it "finite list, negative offset greater than length" $
+        rotate (-10) xs `shouldBe` [3, 4, 5, 6, 1, 2]
+
+    it "infinite list, 0 offset" $
+        take 10 (rotate 0 [1..]) `shouldBe` [1..10]
+    it "ininite list, positive offset" $
+        take 10 (rotate 10 [1..]) `shouldBe` [11..20]
+
+    let bigOffset = 3*10^8
+    it "finite list, huge positive offset" $
+        rotate bigOffset xs `shouldBe` rotate (bigOffset `mod` length xs) xs
+    it "finite list, huge negative offset" $
+        rotate (-bigOffset) xs
+            `shouldBe` rotate ((-bigOffset) `mod` length xs) xs
 
 rotationsSpec :: Spec
-rotationsSpec = undefined
+rotationsSpec = do
+    it "empty list" $ rotations ([] :: [()]) `shouldBe` [[]]
+    it "finite list" $ rotations [1, 2, 3, 4] `shouldBe`
+        [[1, 2, 3, 4], [2, 3, 4, 1], [3, 4, 1, 2], [4, 1, 2, 3]]
+    it "infinite list" $ take 4 (map (take 4) $ rotations [1..]) `shouldBe`
+        [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]]
 
 combinationsOfSizeSpec :: Spec
-combinationsOfSizeSpec = undefined
+combinationsOfSizeSpec = do
+    it "size < 0" $ combinationsOfSize (-1) undefined `shouldBe` ([] :: [[()]])
+    it "size == 0" $ combinationsOfSize 0 undefined `shouldBe` ([[]] :: [[()]])
+    it "empty list" $ do
+        combinationsOfSize 1 [] `shouldBe` ([] :: [[()]])
+        combinationsOfSize 2 [] `shouldBe` ([] :: [[()]])
+    it "singleton list" $ do
+        combinationsOfSize 1 [1] `shouldBe` [[1]]
+        combinationsOfSize 2 [1] `shouldBe` []
+    it "finite list" $ do
+        combinationsOfSize 1 [1, 2, 3, 4] `shouldBe` [[1], [2], [3], [4]]
+        combinationsOfSize 2 [1, 2, 3, 4]
+            `shouldBe` [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+        combinationsOfSize 3 [1, 2, 3, 4]
+            `shouldBe` [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4]]
+        combinationsOfSize 4 [1, 2, 3, 4]
+            `shouldBe` [[1, 2, 3, 4]]
+    it "infinite list" $
+        take 5 (combinationsOfSize 3 [1..])
+            `shouldBe` [[1, 2, 3], [1, 2, 4], [1, 2, 5], [1, 2, 6], [1, 2, 7]]
 
 permutationsOfSizeSpec :: Spec
-permutationsOfSizeSpec = undefined
+permutationsOfSizeSpec = do
+    it "size < 0" $ permutationsOfSize (-1) undefined `shouldBe` ([] :: [[()]])
+    it "size == 0" $ permutationsOfSize 0 undefined `shouldBe` ([[]] :: [[()]])
+    it "empty list" $ do
+        permutationsOfSize 1 [] `shouldBe` ([] :: [[()]])
+        permutationsOfSize 2 [] `shouldBe` ([] :: [[()]])
+    it "singleton list" $ do
+        permutationsOfSize 1 [1] `shouldBe` [[1]]
+        permutationsOfSize 2 [1] `shouldBe` []
+
+    let naive n = sort . (combinationsOfSize n >=> permutations)
+        test n xs = permutationsOfSize n xs `shouldBe` naive n xs
+    it "finite list" $
+        forM_ [1..5] $ \i -> test i [1, 2, 3, 4]
 
 derangementsSpec :: Spec
-derangementsSpec = undefined
+derangementsSpec = do
+    it "empty list" $ derangements [] `shouldBe` ([[]] :: [[()]])
+    it "singleton" $ derangements [1] `shouldBe` []
+    it "list of length 2" $ derangements [1, 2] `shouldBe` [[2, 1]]
+    it "list of length 4" $ derangements [1, 2, 3, 4] `shouldBe`
+        [ [2,1,4,3], [2,3,4,1], [2,4,1,3], [3,1,4,2], [3,4,1,2]
+        , [3,4,2,1], [4,1,2,3], [4,3,1,2], [4,3,2,1]]
